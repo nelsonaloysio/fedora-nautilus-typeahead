68b121257074b1abfad224c4d48d3fd8775e9216
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2024-05-11 14:33:58 +0200

    Added support for Fedora 39/40.

--- build-nautilus-typeahead-rpm.sh
+++ build-nautilus-typeahead-rpm.sh
@@ -6,14 +6,88 @@
 # functionality for Fedora Workstation/Silverblue.
 
 URL="https://github.com/nelsonaloysio/build-nautilus-typeahead-rpm"
-URL_NAUTILUS="https://github.com/GNOME/nautilus/archive/refs/tags/46.1.tar.gz"
-URL_PATCH="https://github.com/lubomir-brindza/nautilus-typeahead/archive/refs/tags/46-beta-0ubuntu3ppa2.tar.gz"
 
 NAME="nautilus"
-VERSION="46.1"
 FEDORA="$(rpm -E %fedora)"
 RELEASE="1"
 ARCH="x86_64"
+FLAGS="--prefix=/usr --buildtype=release -Ddocs=false -Dpackagekit=false"
+
+USAGE="""Usage:
+    $(basename $0) [-h] [-p PATCH_URL] [-n NAUTILUS] [-f FEDORA] [-r RELEASE] [--flags FLAGS] [--unsupported]
+
+Arguments:
+    -h, --help
+        Show this help message and exit.
+    -p, --patch-url PATCH_URL
+        Specify patch URL to obtain. Automatic for Fedora 39/40.
+    -n, --nautilus NAUTILUS
+        Specify Nautilus version. Automatic for Fedora 39/40.
+    -r, --release RELEASE
+        Specify Nautilus release version. Default: '1'.
+    -f, --fedora FEDORA
+        Specify Fedora version. Default: same as running system.
+    --flags FLAGS
+        Specify Nautilus build flags. Replaces default flags.
+        Default: '$FLAGS'.
+    --unsupported
+        Allow building package for unsupported Fedora versions (EOL).
+        Default: only versions 39/40 are supported."""
+
+# Parse arguments.
+while [[ $# -gt 0 ]]; do
+    ARGS+=("$1")
+    case $1 in
+        -h|--help)
+            echo "$USAGE"
+            exit 0
+            ;;
+        -p|--patch-url)
+            URL_PATCH="$2"
+            ARGS+=("$2")
+            shift 2
+            ;;
+        -n|--nautilus)
+            VERSION="$2"
+            ARGS+=("$2")
+            shift 2
+            ;;
+        -r|--release)
+            RELEASE="$2"
+            ARGS+=("$2")
+            shift 2
+            ;;
+        -f|--fedora)
+            FEDORA="$2"
+            ARGS+=("$2")
+            shift 2
+            ;;
+        --flags)
+            FLAGS="$2"
+            ARGS+=("$2")
+            shift 2
+            ;;
+        --unsupported)
+            UNSUPPORTED=1
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+set -- "${ARGS[@]}"
+
+# Check current Fedora version.
+if [ "$FEDORA" = 40 ]; then
+    [ -z "$VERSION" ] && VERSION="46.1"
+    [ -z "$URL_PATH" ] && URL_PATCH="https://github.com/lubomir-brindza/nautilus-typeahead/archive/refs/tags/46-beta-0ubuntu3ppa2.tar.gz"
+elif [ "$FEDORA" = 39 ]; then
+    [ -z "$VERSION" ] && VERSION="45.2.1"
+    [ -z "$URL_PATCH" ] && URL_PATCH="https://aur.archlinux.org/cgit/aur.git/snapshot/aur-524d92c42ea768e5e4ab965511287152ed885d22.tar.gz"
+elif [ "$UNSUPPORTED" != 1 ]; then
+    echo -e "[!] Fedora version $FEDORA is not supported (EOL).\nPass '--unsupported' to ignore this message."
+    exit 1
+fi
 
 # Create RPM build directories
 echo -e "Create RPM build directories..."
@@ -27,15 +101,25 @@ done
 # Install requirements.
 echo -e "\nInstall requirements..."
 sudo dnf install \
+    appstream-devel \
+    desktop-file-utils \
+    'dnf-command(download)' \
+    gcc \
     git \
     gnome-autoar-devel \
+    gnome-desktop4-devel \
     gstreamer1-plugins-base-devel \
+    libadwaita-devel \
+    libappstream-glib \
     libgexiv2-devel \
     libportal-gtk3-devel \
     libportal-gtk4-devel \
     meson \
+    pkgconfig \
+    rpm-build \
     rpmrebuild \
-    tracker-devel
+    tracker-devel \
+    wget
 
 # Create new folder and change directory.
 echo -e "\nCreate new folder and change directory..."
@@ -45,7 +129,7 @@ cd build-nautilus-typeahead-rpm
 
 # Download and extract nautilus.
 echo -e "\nDownload and extract nautilus..."
-wget "$URL_NAUTILUS" -O nautilus.tar.gz
+wget "https://github.com/GNOME/nautilus/archive/refs/tags/${VERSION}.tar.gz" -O nautilus.tar.gz
 tar -xzvf nautilus.tar.gz
 
 # Download and extract nautilus-typeahead patch.
@@ -63,6 +147,12 @@ patch \
     --strip=1 < \
     nautilus-restore-typeahead.patch
 
+# Enable type-ahead functionality by default.
+# https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=nautilus-typeahead
+awk -i inplace \
+    '/type-ahead-search/{c++;} c==1 && /true/{sub("true", "false"); c++;} 1' \
+    ${NAME}-${VERSION}/data/org.gnome.nautilus.gschema.xml
+
 # Change directory.
 echo -e "\nChange directory..."
 mkdir -p ${NAME}-${VERSION}/build &&
@@ -70,20 +160,13 @@ cd ${NAME}-${VERSION}/build
 
 # Setup and build patched nautilus.
 echo -e "\nSetup and build patched nautilus..."
-meson setup \
-    --prefix=/usr \
-    --buildtype=release \
-    -Dpackagekit=false \
-    # -Dselinux=false
-
+meson setup $FLAGS
 ninja
 
 # Download RPM and extract files.
 echo -e "\nDownload RPM and extract files..."
 cd ../..
-
 dnf download ${NAME}-${VERSION}-${RELEASE}.fc${FEDORA}.${ARCH}
-
 rpm2cpio ${NAME}-${VERSION}-${RELEASE}.fc${FEDORA}.${ARCH}.rpm |
 cpio -idmv
 
@@ -126,11 +209,23 @@ mv ${HOME}/rpmbuild/RPMS/x86_64/${NAME}-typeahead-${VERSION}-${RELEASE}.fc${FEDO
 # Remove generated files.
 echo -e "\nRemove generated files..."
 rm -rf build-nautilus-typeahead-rpm
+rm -df \
+    $(find ${HOME}/rpmbuild -type f | grep ${NAME}-${VERSION}-${RELEASE}) \
+    $(find ${HOME}/rpmbuild -type d | grep ${NAME}-${VERSION}-${RELEASE} | sort -r)
 
 # Check if file was built.
-[ -f "${NAME}-typeahead-${VERSION}-${RELEASE}.fc${FEDORA}.${ARCH}.rpm" ] &&
-echo -e "\nSuccessfully built '${NAME}-typeahead-${VERSION}-${RELEASE}.fc${FEDORA}.${ARCH}.rpm'." ||
+[ ! -f "${NAME}-typeahead-${VERSION}-${RELEASE}.fc${FEDORA}.${ARCH}.rpm" ] &&
 echo -e """
-Failed to build '${NAME}-typeahead-${VERSION}-${RELEASE}.fc${FEDORA}.${ARCH}.rpm'.\n
-Please submit an issue with the log of execution to:
-${URL}/issues"""
+Failed to build '${NAME}-typeahead-${VERSION}-${RELEASE}.fc${FEDORA}.${ARCH}.rpm'.
+Please submit an issue with the log of execution if desired to:
+${URL}/issues""" &&
+exit 1
+
+# Print success message and ask to clean dependencies.
+echo -e "\nSuccessfully built '${NAME}-typeahead-${VERSION}-${RELEASE}.fc${FEDORA}.${ARCH}.rpm'.\n"
+echo "Undo last dnf action of installing dependencies? [Y/n]"
+read UNDO
+
+# Clean dependencies.
+[ "${UNDO,,}" = y ] &&
+dnf history undo "$(dnf history list --reverse | tail -n1 | cut -f1 -d\|)
